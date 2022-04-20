8be87362e87e20a27709bd607019cce62e3de9ff
braughtg <braught@dickinson.edu>
2022-04-20 16:45:21 -0400

    change to comp256 for app

--- spinup.bash
+++ spinup.bash
@@ -5,4 +5,4 @@ docker build -t test .
 echo "Done."
 
 echo "Running Docker Container..."
-docker run --rm -it -p 8080:8080 -p8000:8000 --mount type=bind,source="$(pwd)"/webapp,target=/usr/local/tomcat/webapps/webapp --mount type=bind,source="$(pwd)"/northwindAPI,target=/usr/local/tomcat/northwindAPI test
+docker run --rm -it -p 8080:8080 -p8000:8000 --mount type=bind,source="$(pwd)"/comp256,target=/usr/local/tomcat/webapps/comp256 --mount type=bind,source="$(pwd)"/northwindAPI,target=/usr/local/tomcat/northwindAPI test
