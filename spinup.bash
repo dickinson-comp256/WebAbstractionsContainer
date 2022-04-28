26fc19f0378054f4153fc3ed489a481054dc161f
braughtg <braught@dickinson.edu>
2022-04-28 10:47:42 -0400

    Named the container comp256

--- spinup.bash
+++ spinup.bash
@@ -5,4 +5,5 @@ docker build -t test .
 echo "Done."
 
 echo "Running Docker Container..."
-docker run --rm -it -p 8080:8080 -p8000:8000 --mount type=bind,source="$(pwd)"/comp256,target=/usr/local/tomcat/webapps/comp256 --mount type=bind,source="$(pwd)"/northwindAPI,target=/usr/local/tomcat/northwindAPI test
+docker stop comp256
+docker run --rm -it --name comp256 -p 8080:8080 -p8000:8000 --mount type=bind,source="$(pwd)"/comp256,target=/usr/local/tomcat/webapps/comp256 --mount type=bind,source="$(pwd)"/northwindAPI,target=/usr/local/tomcat/northwindAPI test
